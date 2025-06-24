41c55c2643cc184739ef72e83cea90d7992924a4
code2nvim <code2nvim@gmail.com>
2025-06-24 23:53:54 +0800

    init: carrots-gone

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <header>
+        <>header</>
+      </header>
+      <main className="flex grow items-center justify-center bg-green-500">
+        <p>App</p>
+      </main>
+    </div>
+  );
+}
