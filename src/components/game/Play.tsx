c2bbb27b79a14e9d741e8c13ee29b8707d1dfa76
code2nvim <code2nvim@gmail.com>
2025-09-23 11:41:10 +0800

    add targets to GamePlay

--- src/components/game/Play.tsx
+++ src/components/game/Play.tsx
@@ -1,7 +1,18 @@
+import Hole from "/hole.svg";
 import { useGameStatusStore } from "../../store/gameStatus";
 
 export function GamePlay() {
   const { addScore } = useGameStatusStore();
 
-  return <button onClick={addScore}>GamePlay</button>;
+  const targets = Array.from({ length: 9 }, (_, index) => index);
+
+  return (
+    <section className="flex">
+      {targets.map((key) => (
+        <button key={key} onClick={addScore}>
+          <img src={Hole} className="aspect-square" />
+        </button>
+      ))}
+    </section>
+  );
 }
